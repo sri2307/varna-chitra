1c36ad322f13a7f1f0d169dc860b681dd8d862da
msri23 <112919748+msri23@users.noreply.github.com>
2024-03-12 04:08:47 +0530

    tranformations and home page added

--- src/components/shared/MobileNav.tsx
+++ src/components/shared/MobileNav.tsx
@@ -73,7 +73,7 @@ const MobileNav = () => {
         </SignedIn>
 
         <SignedOut>
-            <Button asChild className="button bg-purple-gradient bg-cover">
+            <Button asChild className="button bg-blue-gradient bg-cover">
               <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
